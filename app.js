7e5cd44c63cbc972b691296e1452aa3fa08d6aad
shivamiqa <shivamk898@gmail.com>
2020-05-14 20:01:10 +0530

    new files

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
-// ********** set date ************
-// select span
 const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
-// ********** close links ************
+
 const navToggle = document.querySelector(".nav-toggle");
 const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
@@ -26,7 +19,7 @@ navToggle.addEventListener("click", function () {
   // console.log(linksContainer.getBoundingClientRect());
 });
 
-// ********** fixed navbar ************
+
 
 const navbar = document.getElementById("nav");
 const topLink = document.querySelector(".top-link");
@@ -50,7 +43,7 @@ window.addEventListener("scroll", function () {
   }
 });
 
-// ********** smooth scroll ************
+
 // select links
 const scrollLinks = document.querySelectorAll(".scroll-link");
 scrollLinks.forEach((link) => {
